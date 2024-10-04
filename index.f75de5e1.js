7efddbd30e2eaaee8901f097a9433f0c108ca2c0
Korshun <dmitriy.kor91@gmail.com>
2024-10-04 14:24:15 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,29 @@
+"use strict";
+function waitFor(element, eventName) {
+    const generalPromise = new Promise((resolve, reject)=>{
+        element.addEventListener(eventName, (e)=>{
+            const message = `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`;
+            resolve(message);
+        });
+    });
+    return generalPromise;
+}
+const printMessage = (message)=>{
+    const div = document.createElement("div");
+    div.setAttribute("class", "message");
+    div.textContent = message;
+    document.body.appendChild(div);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
